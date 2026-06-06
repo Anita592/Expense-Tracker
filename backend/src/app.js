1c4f39f7926938bd24d29de97190623043bccc82
Anita592 <ao69964@ubt-uni.net>
2026-06-06 19:16:03 +0200

    fix packeg.json and app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(Server running on port ${PORT}));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
